ff038ebf65ad894e7d3090d436df56e65b0aa9a9
rbpasker <bob@pasker.net>
2013-08-08 18:15:56 -0400

    Add module-level

---
+++ tengah_sdk/__init__.py
@@ -0,0 +1,5 @@
+from metadata_constants import MetadataConstants
+from metadata_attribute_constants import MetadataAttributeConstants
+
+__all__ = ['metadata_constants', 'metadata_attribute_constants']
+
--- tengah_sdk/metadata_attribute_constants.py
+++ tengah_sdk/metadata_attribute_constants.py
@@ -1,20 +1,21 @@
-FIELDS = {
-    'TYPE' : 'attribute_type',
-    'INFO' : 'info',
-    'DATA' : 'data',
-    'CID'  : 'cid'
-}
-
-TYPES = {
-    'AMOUNT' : 'amount',
-    'DATE': 'date',
-    'EMAIL': 'email',
-    'LOCCOORD': 'location_coordinate',
-    'LOCNAME': 'location_name',
-    'NAME': 'name',
-    'PHONE': 'phone',
-    'TAG': 'tag',
-    'TEXT': 'text',
-    'URL': 'url',
-    'USERID' : 'userid',
-}
+class MetadataAttributeConstants(object):
+    FIELDS = {
+        'TYPE' : 'attribute_type',
+        'INFO' : 'info',
+        'DATA' : 'data',
+        'CID'  : 'cid'
+    }
+    
+    TYPES = {
+        'AMOUNT' : 'amount',
+        'DATE': 'date',
+        'EMAIL': 'email',
+        'LOCCOORD': 'location_coordinate',
+        'LOCNAME': 'location_name',
+        'NAME': 'name',
+        'PHONE': 'phone',
+        'TAG': 'tag',
+        'TEXT': 'text',
+        'URL': 'url',
+        'USERID' : 'userid',
+    }
