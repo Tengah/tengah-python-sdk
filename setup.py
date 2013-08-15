6741a73f02e01a7d6ebfb5443f8ae435031fd34d
rbpasker <bob@pasker.net>
2013-08-15 18:09:14 -0400

    bump version to 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tengah SDK',
-    version='0.1',
+    version='0.2',
     url='https//github.com/tengah/tengah-python-sdk',
     packages=['tengah_sdk'],
     install_requires=['requests']
