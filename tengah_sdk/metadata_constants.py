8acedb661916dc07e4d9bd93b29609a24ca8be1c
Paul Young <paulyoungonline@gmail.com>
2014-01-21 15:11:51 -0500

    Updated property names.

--- tengah_sdk/metadata_constants.py
+++ tengah_sdk/metadata_constants.py
@@ -1,14 +1,14 @@
 FIELDS = {
     "ID"         : 'id',
-    "OWNER"      : 'owner',
-    "SERVICE"    : 'service',
-    'ORIGINAL_DOC_ID' : 'original_doc_id',
-    'METADATA_TYPE' : 'metadata_type',
+    "OWNER_ID"      : 'owner_id',
+    "SERVICE_NAME"    : 'service_name',
+    'ORIGINAL_DOC_REF' : 'original_doc_ref',
+    'KIND' : 'kind',
     "ATTRIBUTES" : 'attributes',
     "IS_DOWNLOADABLE" : 'is_downloadable',
 }
 
-INFO = {
+NAME = {
     # Base
     'CREATED_AT' : 'created_at',
     'DESCRIPTION' : 'description',
@@ -19,7 +19,7 @@ INFO = {
     'CONTENTS' : 'contents',
     'TYPE' : 'type',
     'STATUS_TYPE' : 'status_type',
-    
+
     # Message
     'CC' : 'cc',
     'FROM' : 'from',
@@ -32,7 +32,7 @@ INFO = {
     'MESSAGE' : 'message',
     'SENT_AT' : 'sent_at',
     'EMAIL' : 'email',
-    
+
     # File
     'FILENAME'   : 'filename',
     'FILESIZE_IN_BYTES' : 'filesize_in_bytes',
@@ -48,7 +48,7 @@ INFO = {
     'MODIFIED_BY'  :'modified_by',
     'PURGED_AT' : 'purged_at',
     'DELETED_AT' : 'deleted_at',
-    
+
     # Image
     'DEPTH' : 'image_depth',
     'HEIGHT' : 'image_height',
@@ -61,7 +61,7 @@ INFO = {
     'FOLDER' : 'folder',
     'MAILINGLIST': 'mailinglist',
     'CAPTION' : 'caption',
-    
+
     # EXIF
     'EXIF_CAPTURED' : 'exif_captured',
     'EXIF_ORIENTATION' : 'exif_orientation',
@@ -70,19 +70,19 @@ INFO = {
     'IMAGE_WIDTH' : 'exif_image_width',
     'IMAGE_XRES' : 'exif_image_xres',
     'IMAGE_YRES' : 'exif_image_yres',
-    
+
     # Event
     'ATTENDEE' : 'attendee',
     'END_AT' : 'end_at',
     'ORGANIZER' : 'organizer',
     'START_AT' : 'start_at',
     'SUMMARY' : 'summary',
-    
+
     # OpenCalais
     'ENTITY' : 'entity',
     'TOPIC'  : 'topic',
     'SOCIAL_TAG' : 'social_tag',
-    
+
     # Contact
     'FRIEND' : 'friend',
     'DISPLAY_NAME' : 'display_name',
@@ -105,7 +105,7 @@ INFO = {
     'PAGER' : 'pager',
     'WORK' : 'work',
     'ORG_NAME' : 'organization',
-    
+
     # Personal
     'CALORIES' : 'calories',
     'STEPS' : 'steps',
@@ -138,29 +138,16 @@ FILES = {
     'VIDEO' : 'video',
 }
 
-SERVICES = {
-    'CONTEXTIO' : 'contextio',
-    'DROPBOX' : 'dropbox',
-    'FACEBOOK' : 'facebook',
-    'FITBIT' : 'fitbit',
-    'GOOGLEDRIVE' : 'googledrive',
-    'ICAL' : 'ical',
-    'IMAGES' : 'images',
-    'LINKEDIN' : 'linkedin',
-    'TWITTER' : 'twitter'
-}
-
 DOCUMENT_FILES = (
     FILES['PRESENTATION'],
     FILES['SPREADSHEET'],
     FILES['TEXT'],
     FILES['PDF'],
 )
-    
+
 _ALL_ = {
     'FIELDS' : FIELDS,
     'FILES' : FILES,
-    'INFO' : INFO,
-    'SERVICES' : SERVICES,
+    'NAME' : NAME,
     'KINDS' : KINDS,
 }
